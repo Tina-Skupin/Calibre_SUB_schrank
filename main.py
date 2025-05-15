89462fe00cde8e73f5fa6e388e4a0e0a3a55dfad
Tina-Skupin <tskupin32@gmail.com>
2025-05-15 11:15:55 +0200

    proof of concept

--- main.py
+++ main.py
@@ -9,13 +9,22 @@ screen = pygame.display.set_mode((1800, 920))
 clock = pygame.time.Clock()
 running = True
 
+#creating the shelf
 Rect = pygame.Rect
 shelf1 = Rect(60, 600, 1600, 15)
+
+#creating the books
 book = Rect(100, 300, 70, 300)
 book2 = Rect(170, 250, 80, 350)
 book3 = Rect(250, 400, 60, 200)
 
-
+#creating the text
+# Choose a font
+font = pygame.font.SysFont('Arial', 40)
+# Render the text
+text = font.render('Valkyrie- zurück ins jetzt', True, (255, 155, 0))
+# Rotate the text
+text = pygame.transform.rotate(text, 90)
 
 while running:
     # poll for events
@@ -31,6 +40,11 @@ while running:
     pygame.draw.rect(screen, (0, 255, 16), book)
     pygame.draw.rect(screen, (207, 166, 16), book2)
     pygame.draw.rect(screen, (0, 255, 16), book3)
+
+    # Display the text
+    rect = text.get_rect(center=(130, 500))
+    screen.blit(text, rect)
+    pygame.display.flip()
 
 
     # RENDER YOUR GAME HERE
